0daa79d3393e8d4cab6ea942947171f765a7b373
Aleksandar Cambas <aleksandarcambas@gmail.com>
2024-08-06 14:59:13 +0200

    moa

--- .config/nvim/lua/acambas/plugins/snapshot.lua
+++ .config/nvim/lua/acambas/plugins/snapshot.lua
@@ -1,13 +1,13 @@
 return {
-	"mistricky/codesnap.nvim",
-	build = "make",
-	enabled = "false",
-	event = "VeryLazy",
-	config = function()
-		require("codesnap").setup({
-			bg_color = "#535c68",
-			has_breadcrumbs = false,
-			mac_window_bar = false,
-		})
-	end,
+  "mistricky/codesnap.nvim",
+  build = "make",
+  enabled = false,
+  event = "VeryLazy",
+  config = function()
+    require("codesnap").setup({
+      bg_color = "#535c68",
+      has_breadcrumbs = false,
+      mac_window_bar = false,
+    })
+  end,
 }
--- .wezterm.lua
+++ .wezterm.lua
@@ -11,18 +11,21 @@ config.color_scheme = "rose-pine-moon"
 config.hide_tab_bar_if_only_one_tab = true
 config.audible_bell = "Disabled"
 config.keys = {
-	-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
-	{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
-	-- Make Option-Right equivalent to Alt-f; forward-word
-	{ key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
+  -- { key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
+  -- { key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
+  -- { mods = "CMD", key = "LeftArrow",  action = wezterm.action.SendKey({ mods = "CTRL", key = "a" }) },
+  { mods = "CMD", key = "RightArrow", action = wezterm.action.SendKey({ mods = "CTRL", key = "e" }) },
+  { mods = "CMD", key = "Backspace",  action = wezterm.action.SendKey({ mods = "CTRL", key = "u" }) },
+  { mods = "OPT", key = "LeftArrow",  action = wezterm.action.SendKey({ mods = "ALT", key = "b" }) },
+  { mods = "OPT", key = "RightArrow", action = wezterm.action.SendKey({ mods = "ALT", key = "f" }) },
 }
 config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
 
 config.window_padding = {
-	left = 15,
-	right = 15,
-	top = 5,
-	bottom = 5,
+  left = 15,
+  right = 15,
+  top = 5,
+  bottom = 5,
 }
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.send_composed_key_when_left_alt_is_pressed = false
