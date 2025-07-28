73e2a7f0e088c287d2dae3b1b1e7e8054ed6d21c
Aleksandar Cambas <aleksandarcambas@gmail.com>
2025-07-28 22:21:42 +0200

    keymaps cleanup

--- .config/nvim/lua/acambas/keymaps.lua
+++ .config/nvim/lua/acambas/keymaps.lua
@@ -1,58 +1,49 @@
+-- ============================================================================
+-- LEADER KEYS
+-- ============================================================================
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- Common options
 local noremap_silent = { noremap = true, silent = true }
 
-vim.keymap.set({ "i", "n" }, "<C-s>", "<Esc>:silent update!<CR>", noremap_silent) -- save file
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", noremap_silent) -- move selection up
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", noremap_silent) -- move selection up
-vim.keymap.set("n", "M", "gM", noremap_silent) -- Move to the middle of the line in normal mode
-vim.keymap.set("n", "<C-D>", "<C-D>zz") -- Scroll down half a page and center the cursor line
-vim.keymap.set("n", "<C-U>", "<C-U>zz") -- Scroll up half a page and center the cursor line
-vim.keymap.set({ "n", "v" }, "H", "^") -- Move to the start of the line in normal and visual mode
-vim.keymap.set({ "n" }, "L", "$") -- Move to the end of the line in normal and visual mode
-vim.keymap.set({ "v" }, "L", "$h") -- Move to the end of the line in normal and visual mode
-vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true }) -- Disable the space key in normal and visual mode
-vim.keymap.set("n", "S", "<nop>") -- Disable the S key in normal mode
-vim.keymap.set({ "n", "v" }, "c", '"xc') -- Cut to clipboard in normal and visual mode
-vim.keymap.set({ "v" }, "p", "P") -- Paste before the cursor in visual mode
-vim.keymap.set("n", "ga", "ggVG") -- Select all text in the file
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n><C-w>h", { silent = true }) -- Exit terminal mode with Esc
-vim.keymap.set("i", "<m-backspace>", "<C-w>") -- Delete the previous word in insert mode with Alt+Backspace
-vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>", noremap_silent) -- Clear search highlight with Esc in normal mode
-vim.keymap.set("n", "U", "<C-r>", { remap = false }) -- Use `u` to undo, use `U` to redo
-vim.keymap.set("n", "n", "nzz", { remap = false }) -- Search next with n and center the cursor line
-vim.keymap.set("n", "N", "Nzz", { remap = false }) -- Search previous with N and center the cursor line
-vim.keymap.set("n", "*", "*zz", { remap = false }) -- Search the word under the cursor with * and center the cursor line
-vim.keymap.set("n", "<C-q>", "<C-w>q", { silent = true }) -- Close the current window with Ctrl+q
-vim.keymap.set("v", "'", [[<right>:s/\%V\(.*\%V\)/'\1'/ <CR>]], { desc = "Surround selection with '" }) -- Surround the visual selection with ''
-vim.keymap.set("v", '"', [[<right>:s/\%V\(.*\)\%V/"\1"/ <CR>]], { desc = 'Surround selection with "' }) -- Surround the visual selection with ''
-vim.keymap.set("v", "`", [[<right>:s/\%V\(.*\)\%V/`\1`/ <CR>]], { desc = "Surround selection with `" }) -- Surround the visual selection with ``
-vim.keymap.set("v", "{", [[<right>:s/\%V\(.*\)\%V/{\1}/ <CR>]], { desc = "Surround selection with {" }) -- Surround the visual selection with {}
-vim.keymap.set("v", "[", [[<right>:s/\%V\(.*\)\%V/[\1]/ <CR>]], { desc = "Surround selection with [" }) -- Surround the visual selection with []
-vim.keymap.set("v", "(", [[<right>:s/\%V\(.*\)\%V/\(\1\)/ <CR>]], { desc = "Surround selection with [" }) -- Surround the visual selection with []
-vim.keymap.set("v", "n", 'y/<C-r>"<CR>N') -- Search the visual selection
-vim.keymap.set({ "n", "v", "i" }, "<C-g>", "<esc>") --
+-- ============================================================================
+-- DISABLED KEYS
+-- ============================================================================
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+vim.keymap.set("n", "S", "<nop>")
 
+-- ============================================================================
+-- FILE OPERATIONS
+-- ============================================================================
+vim.keymap.set({ "i", "n" }, "<C-s>", "<Esc>:silent update!<CR>", noremap_silent)
+
+-- ============================================================================
+-- MOVEMENT AND NAVIGATION
+-- ============================================================================
+-- Enhanced line movement
+vim.keymap.set({ "n", "v" }, "H", "^")
+vim.keymap.set("n", "L", "$")
+vim.keymap.set("v", "L", "$h")
+vim.keymap.set("n", "M", "gM", noremap_silent)
+
+-- Centered scrolling
+vim.keymap.set("n", "<C-D>", "<C-D>zz")
+vim.keymap.set("n", "<C-U>", "<C-U>zz")
+
+-- Buffer navigation
 vim.keymap.set("n", "<C-t>", ":b#<CR>", { silent = true })
 
--- exit terminal mode with alt-escape
-vim.keymap.set("t", "<M-Esc>", "<C-\\><C-n >", { silent = true })
+-- ============================================================================
+-- SEARCH AND REPLACE
+-- ============================================================================
+vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>", noremap_silent)
+vim.keymap.set("n", "n", "nzz", { remap = false })
+vim.keymap.set("n", "N", "Nzz", { remap = false })
+vim.keymap.set("n", "*", "*zz", { remap = false })
+vim.keymap.set("v", "n", 'y/<C-r>"<CR>N')
 
--- Resize window using Alt and arrow keys
-vim.keymap.set("n", "<S-Left>", "<c-w>5<")
-vim.keymap.set("n", "<S-Right>", "<c-w>5>")
-vim.keymap.set("n", "<S-Up>", "<c-w>+")
-vim.keymap.set("n", "<S-Down>", "<c-w>-")
-
--- keymap that deletes the current line and puts the cursor in the right place
-vim.keymap.set("n", "dd", function()
-	if vim.fn.getline(".") == "" then
-		return '"_dd'
-	end
-	return "dd"
-end, { expr = true })
-
+-- Visual selection search and replace
 table.unpack = table.unpack or unpack
 local function get_visual()
 	local _, ls, cs = table.unpack(vim.fn.getpos("v"))
@@ -65,3 +56,58 @@ vim.keymap.set("v", "<C-r>", function()
 	pattern = vim.fn.substitute(vim.fn.escape(pattern, "^$.*\\/~[]"), "\n", "\\n", "g")
 	vim.api.nvim_input("<Esc>:%s/" .. pattern .. "/" .. pattern .. "/gc<Left><Left><Left>")
 end)
+
+-- ============================================================================
+-- TEXT MANIPULATION
+-- ============================================================================
+-- Move selected lines
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", noremap_silent)
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", noremap_silent)
+
+-- Selection and clipboard
+vim.keymap.set("n", "ga", "ggVG")
+vim.keymap.set({ "n", "v" }, "c", '"xc')
+vim.keymap.set("v", "p", "P")
+
+-- Undo/Redo
+vim.keymap.set("n", "U", "<C-r>", { remap = false })
+
+-- Smart line deletion
+vim.keymap.set("n", "dd", function()
+	if vim.fn.getline(".") == "" then
+		return '"_dd'
+	end
+	return "dd"
+end, { expr = true })
+
+-- ============================================================================
+-- SURROUND OPERATIONS
+-- ============================================================================
+vim.keymap.set("v", "'", [[<right>:s/\%V\(.*\%V\)/'\1'/ <CR>]], { desc = "Surround with single quotes" })
+vim.keymap.set("v", '"', [[<right>:s/\%V\(.*\)\%V/"\1"/ <CR>]], { desc = "Surround with double quotes" })
+vim.keymap.set("v", "`", [[<right>:s/\%V\(.*\)\%V/`\1`/ <CR>]], { desc = "Surround with backticks" })
+vim.keymap.set("v", "{", [[<right>:s/\%V\(.*\)\%V/{\1}/ <CR>]], { desc = "Surround with curly braces" })
+vim.keymap.set("v", "[", [[<right>:s/\%V\(.*\)\%V/[\1]/ <CR>]], { desc = "Surround with square brackets" })
+vim.keymap.set("v", "(", [[<right>:s/\%V\(.*\)\%V/\(\1\)/ <CR>]], { desc = "Surround with parentheses" })
+
+-- ============================================================================
+-- WINDOW MANAGEMENT
+-- ============================================================================
+vim.keymap.set("n", "<C-q>", "<C-w>q", { silent = true })
+
+-- Window resizing
+vim.keymap.set("n", "<S-Left>", "<c-w>5<")
+vim.keymap.set("n", "<S-Right>", "<c-w>5>")
+vim.keymap.set("n", "<S-Up>", "<c-w>+")
+vim.keymap.set("n", "<S-Down>", "<c-w>-")
+
+-- ============================================================================
+-- TERMINAL MODE
+-- ============================================================================
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n><C-w>h", { silent = true })
+vim.keymap.set("t", "<M-Esc>", "<C-\\><C-n>", { silent = true })
+
+-- ============================================================================
+-- INSERT MODE ENHANCEMENTS
+-- ============================================================================
+vim.keymap.set("i", "<m-backspace>", "<C-w>")
